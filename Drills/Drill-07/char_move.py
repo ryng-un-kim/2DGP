57c9c9cfe62755de29f89190ace9a67c5ebf6fd3
ryung-un-kim(Nbpc) <fud007@naver.com>
2018-10-02 19:07:00 +0900

    drill7 step1

--- Drills/Drill-07/char_move.py
+++ Drills/Drill-07/char_move.py
@@ -1,14 +1,18 @@
 from pico2d import*
 import random
 
-W, H = 800, 600
+W, H = 1024, 768
 
 open_canvas()
 
 kpu_ground = load_image("kpu_ground.png")
 character = load_image("animation_sheet.png")
+Idle = load_image("character.png")
 
-def character_random_move():
+def character_random_move(p1, p2):
+    pass
+
+while True:
     pass
 
 close_canvas()
