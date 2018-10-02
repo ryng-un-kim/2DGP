c7a72181816bb8e2a08b72751487a0354ddb6f30
ryung-un-kim(Nbpc) <fud007@naver.com>
2018-10-02 19:10:20 +0900

    drill7 step2

--- Drills/Drill-07/char_move.py
+++ Drills/Drill-07/char_move.py
@@ -10,7 +10,12 @@ character = load_image("animation_sheet.png")
 Idle = load_image("character.png")
 
 def character_random_move(p1, p2):
-    pass
+    frame = 0
+    for i in range(0, 100+1, 10):
+        t = i/100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        
 
 while True:
     pass
