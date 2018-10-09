ccf1c2fc4aa080b14549929b7a21336a6be8cb93
ryung-un-kim(Nbpc) <fud007@naver.com>
2018-10-09 16:59:46 +0900

    drill09 step2

--- Drills/Drill-09/boy_grass_object.py
+++ Drills/Drill-09/boy_grass_object.py
@@ -5,21 +5,24 @@ from pico2d import *
 
 class Boy():
     def __init__(self):
-        pass
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
 
     def update(self):
-        pass
+        self.frame = (self.frame + 1) % 8
+        self.x += 5
 
     def draw(self):
-        pass
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
 
 class Grass():
     def __init__(self):
-        pass
+        self.image = load_image('grass.png')
 
     def draw(self):
-        pass
+        self.image.draw(400, 30)
 
 
 class Ball21():
@@ -55,6 +58,16 @@ boy = Boy()
 grass = Grass()
 running = True;
 # game main loop code
+
+
+while running:
+    handle_events()
+    boy.update()
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
+    delay(0.05)
 
 # finalization code
 close_canvas()
