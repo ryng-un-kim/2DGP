d7a045f70eb326ff94dd7d7d625029123a554181
Kim Ryung Un <gongjoa@gmail.com>
2018-10-15 20:06:58 +0900

    Drill10-1 step0

---
+++ Drills/Drill-10-1/pause_state.py
@@ -0,0 +1,32 @@
+import game_framework
+from pico2d import *
+import main_state
+
+name = "PauseState"
+
+image = None
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def update():
+    pass
+
+
+def handle_events():
+    events = get_events()
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
